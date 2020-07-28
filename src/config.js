245f679394ae5f0d8630334ef6d51fb3785ef7da
mayankpande88 <mayankpande88@gmail.com>
2020-07-28 21:23:40 +0530

    added my first blog

--- src/config.js
+++ src/config.js
@@ -1,24 +1,24 @@
 export const config = {
   // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
-  githubConvertedToken: "MzM2N2YxZDRjNDk5MmQ4MGVjNzQwYmM5Y2FlZWYyNDE0ZGQ4NWFiYQ==",
+  githubConvertedToken: "ZWUyMjdiNGRhOWU4NmUwYTM2ODc0ZDk2ZGViZGUwZTdmMDI4NjYyZA==",
 
   // Your Github UserName
-  githubUserName: "saadpasta",
+  githubUserName: "mayankpande88",
 
   // Your Github Repo Name Where You Have your issues as Blog
-  githubRepo: "react-blog-github",
+  githubRepo: "my-learnings",
 
   // Set it to true if you have a Github app to add to this project
   // and fill the client ID & secret
-  enableOAuth: true,
-  OAuthClientID: 'Iv1.9f40fd53257d4102',
-  OAuthSecret: 'cc3db76ffe9848c92d83172807eececa79f0eb22',
+  enableOAuth: false,
+  OAuthClientID: '',
+  OAuthSecret: '',
 
   // Your Personal Blog Title
-  title : "Saad Pasta" ,
+  title : "Mayank Pande" ,
 
   // Your Personal Blog Subtitle
-  subtitle : "Software Developer",
+  subtitle : "My learnings/ Techincal blogs",
 
   // Header customization
   header: {
